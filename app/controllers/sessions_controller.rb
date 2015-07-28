d1750a5edb4e0a06d6e8faab713823eb60551b2a
suomiseth <suomiseth@gmail.com>
2015-07-27 22:11:53 -0400

    facebook-koala gem working, but only gets user info and name--can't get posts, yet--user model and migration also fixed to handle a user oauth token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,13 @@
 class SessionsController < ApplicationController
 
   def create
-    # binding.pry
     @user = User.find_or_create_from_auth_hash(auth_hash)
-
-    @user = session[:user_id] = @user.id
+    session[:user_id] = @user.id
     redirect_to root_path
-    
   end 
-
-
 
   protected
   def auth_hash
     request.env['omniauth.auth']
   end
-
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
- provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+ provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {scope: "email, user_posts"}
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
