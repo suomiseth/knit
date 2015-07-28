541ade3ff1387f7d00444f369707d65a6101c1f4
suomiseth <suomiseth@gmail.com>
2015-07-28 19:15:21 -0400

    list a list of recent facebook posts--these are for the logged in user... i don't know if we can show others' posts.. i'm going to try to make embedded post-links later, but i wanted to get something on the page

--- app/models/facebook_connection.rb
+++ app/models/facebook_connection.rb
@@ -1,7 +1,22 @@
 class FacebookConnection
+  attr_reader :client
 
-  def initialize(oauth_token)
-    @facebook ||= Koala::Facebook::API.new(oauth_token)
+  def initialize(user)
+    @client ||= Koala::Facebook::API.new(user.facebook_auth_token)
+  end
+
+  def posts
+    # not sure if the "me" message here works for other people
+    @client.get_object("me?fields=posts")
+  end
+
+  def post_urls
+    @posts = posts
+    # binding.pry
+    @posts["posts"]["data"].map do |post|
+      ids = post["id"].split("_")
+      "https://facebook.com/#{ids[0]}/posts/#{ids[1]}"
+    end
   end
 
 end
