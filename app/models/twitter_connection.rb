ff2e73921994cc635b9be99e58dd77cac11fcaf9
mitchellhart <mitchellhart@gmail.com>
2015-07-28 10:21:46 -0400

    adds twitter api connection class

---
+++ app/models/twitter_connection.rb
@@ -0,0 +1,16 @@
+class TwitterConnection 
+  attr_reader :client
+
+  def initialize
+    @client = create_client
+  end
+
+  def create_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+end
+
